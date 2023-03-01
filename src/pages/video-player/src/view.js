97631b2390319f5a61a8dba35500baf36161f0dd
Renato <renatomarquesteles@gmail.com>
2023-03-01 11:49:01 -0300

    feat: adds function to convert video into image

--- src/pages/video-player/src/view.js
+++ src/pages/video-player/src/view.js
@@ -1,6 +1,21 @@
 export default class View {
   #initButton = document.querySelector('#init')
   #statusElement = document.querySelector('#status')
+  #videoFrameCanvas = document.createElement('canvas')
+  #canvasContext = this.#videoFrameCanvas.getContext('2d', {
+    willReadFrequently: true,
+  })
+
+  // Converts video into a canvas image
+  getVideoFrame(video) {
+    const canvas = this.#videoFrameCanvas
+    const [width, height] = [video.videoWidth, video.videoHeight]
+    canvas.width = width
+    canvas.height = height
+
+    this.#canvasContext.drawImage(video, 0, 0, width, height)
+    return this.#canvasContext.getImageData(0, 0, width, height)
+  }
 
   enableButton() {
     this.#initButton.disabled = false
